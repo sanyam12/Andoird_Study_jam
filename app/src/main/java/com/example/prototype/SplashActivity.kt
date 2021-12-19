741799d79e04b77a9ee19f5fa4ef91fe0b2a590b
sanyam12 <huBboj-nuvgun-7deksy>
2021-12-20 00:34:06 +0530

    splash completed

---
+++ app/src/main/java/com/example/prototype/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.widget.VideoView
+
+class SplashActivity : AppCompatActivity() {
+
+    val t:Long = 4500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(intent)
+            val intent = Intent(this@SplashActivity, PostActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, t)
+
+
+    }
+}
